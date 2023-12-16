741f1482d9cdab54a61d39503913a33fa25b155f
Brian Martin Rios Nicoli <112521907+KanaRC95@users.noreply.github.com>
2023-12-16 12:48:32 -0300

    Add files via upload

---
+++ final/script.js
@@ -0,0 +1,21 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxjJB0GN7qNM93P5yUyxym5Wh4bY7dg426szXVO86kVDIEBjmHM4QjGR8VkfHHcmbMB/exec';
+const form = document.forms['form-inscripcion'];
+
+
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { 
+    redirect: "follow", 
+    method: 'POST',
+    mode: 'cors', 
+    body: new FormData(form)})
+  .then(response => alert("Su inscripcion ha sido registrada con Exito!" ))
+  .then(() => { window.location.reload(); })
+  .catch(error => console.error('Error!', error.message))
+});
+
+const fechaActual = new Date();
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById("fecha").defaultValue = fechaActual.getDate()+'-'+fechaActual.getMonth()+'-'+fechaActual.getFullYear();
+});
